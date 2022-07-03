4bc4053719faf17f2e1a17bdd2aea73d2af99191
aurindo10 <aurindo15@gmail.com>
2022-07-03 11:38:49 -0300

    teste

--- server.js
+++
@@ -1,5 +0,0 @@
-require ('dotenv').config();
-const http =require ('http');
-const app = require('./app')
-const server = http.createServer(app);
-server.listen(process.env.port, ()=>{console.log ("Server Running")});
